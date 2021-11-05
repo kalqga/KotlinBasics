446469316b8375d1a0242c462b93c7c73a92deb1
mialskywalker <kalqga123@gmail.com>
2021-11-05 13:54:43 +0200

    fund

--- app/src/main/java/com/example/kotlinbasics/Fundamentals.kt
+++ app/src/main/java/com/example/kotlinbasics/Fundamentals.kt
@@ -1,2 +1,18 @@
 package com.example.kotlinbasics
 
+fun main(){
+
+    // val numbers: IntArray = intArrayOf(1,2,3,4,5,6)
+    // or
+    // val numbers = intArrayOf(1,2,3,4,5,6)
+    // or
+    val numbers = arrayOf(1,2,3,4,5,6)
+
+    println(numbers.contentToString())
+
+    for(element in numbers){
+        print(element)
+    }
+
+
+}
